504dbec9aaa59a0e5bf7f1429b3ec1b43c064510
Amina Sultanova <aminasultanova007@gmail.com>
2025-04-29 00:39:29 +0600

    methods added into UserDAO class

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -1,4 +1,120 @@
 package dao;
 
-public class UserDAO {
+import model.Librarian;
+import model.Reader;
+import model.User;
+import util.DatabaseConnection;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO extends BaseDAO<User> {
+    @Override
+    public User getById(int id) throws SQLException {
+        String sql = "SELECT * FROM users WHERE id = ?";
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return mapResultSetToUser(rs);
+            }
+            return null;
+        }
+    }
+
+    @Override
+    public List<User> getAll() throws SQLException {
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+
+        try (Connection conn = getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                users.add(mapResultSetToUser(rs));
+            }
+        }
+        return users;
+    }
+
+    @Override
+    public int create(User user) throws SQLException {
+        String sql = "INSERT INTO users (username, password, type) VALUES (?, ?, ?) RETURNING id";
+
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getPassword());
+            stmt.setString(3, user.getRole());
+
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+            throw new SQLException("User creation failed");
+        }
+    }
+
+    @Override
+    public boolean update(User user) throws SQLException {
+        String sql = "UPDATE users SET username = ?, password = ?, type = ? WHERE id = ?";
+
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getPassword());
+            stmt.setString(3, user.getRole());
+            stmt.setInt(4, user.getId());
+
+            return stmt.executeUpdate() > 0;
+        }
+    }
+
+    @Override
+    public boolean delete(int id) throws SQLException {
+        String sql = "DELETE FROM users WHERE id = ?";
+
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+            return stmt.executeUpdate() > 0;
+        }
+    }
+
+    public User getUserByUsername(String username) throws SQLException {
+        String sql = "SELECT * FROM users WHERE username = ?";
+
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return mapResultSetToUser(rs);
+            }
+            return null;
+        }
+    }
+
+    private User mapResultSetToUser(ResultSet rs) throws SQLException {
+        User user;
+        String type = rs.getString("type");
+
+        if ("librarian".equalsIgnoreCase(type)) {
+            user = new Librarian(rs.getString("username"), rs.getString("password"));
+        } else {
+            user = new Reader(rs.getString("username"), rs.getString("password"));
+        }
+
+        user.setId(rs.getInt("id"));
+        return user;
+    }
 }
